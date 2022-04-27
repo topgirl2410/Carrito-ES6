de734f0243705bf688ef8e087f8e27873a390a15
topgirl2410 <maria.topgirl82@gmail.com>
2022-04-27 16:39:10 +0200

    mostrando en el carrito el curso seleccionado

--- 01-carrito-compra/js/app.js
+++ 01-carrito-compra/js/app.js
@@ -30,10 +30,9 @@ function agregarCurso(e) {
     }
 }
 
-
 // Lee el contenido del HTML al que le dimos click y extrae la información del curso
 
-function leerDatosCurso(curso){
+function leerDatosCurso(curso) {
 
     // Crear un objeto con el contenido del curso actual 
     const infoCurso = {
@@ -45,10 +44,42 @@ function leerDatosCurso(curso){
     }
 
     // Agrega elementos al arreglo de carrito 
-    articulosCarrito = [...articulosCarrito, infoCurso];
+    articulosCarrito = [...articulosCarrito, infoCurso]; // Agregamos una copia del carrito original vacio e incluimos infoCurso;
 
     console.log(articulosCarrito)
 
+    carritoHTML();
+
 }
 
+// Muestra el carrito de compras en el HTML 
 
+function carritoHTML() {
+
+    // Limpiar el HTML 
+    limpiarHTML();
+
+    // Recorre el carrito y genera el HTML 
+
+    articulosCarrito.forEach((curso) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>     
+            ${curso.titulo}
+        </td>
+        `;
+
+        // Agrega el HTML del carrito en el tbody
+
+        contenedorCarrito.appendChild(row);
+    });
+}
+
+// Elimina los cursos del tbody
+
+function limpiarHTML() {
+
+    while (contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
+}
